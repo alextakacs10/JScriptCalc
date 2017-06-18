a2aa20af8aafaa6610cbdca61d179f281bce4658
Alex Takács <alextakacs10@gmail.com>
2017-06-18 21:26:23 +0200

    The button C now clears just the last character, while CE clears the whole input

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 var buttons = document.querySelectorAll("#calc span");
 var operators = ['÷','x','-','+'];
+var decOn = false;
 
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function(e) {
         var input = document.querySelector('.screen');
         var inputValue = input.innerHTML;
         var buttonValue = this.innerHTML;
+        var lastCharacter = inputValue[inputValue.length-1];
 
         if (buttonValue === 'C') {
             input.innerHTML = "";
-        } else if (buttonValue === "=") {
+        }
+        
+        else if (buttonValue === "CE") {
+            input.innerHTML = inputValue.replace(lastCharacter, "");
+        }
+        
+        else if (buttonValue === "=") {
             var toCalculate = inputValue;
             toCalculate = toCalculate.replace(/x/g,'*').replace(/÷/g,'/');
 
             if (toCalculate) {
                 input.innerHTML = eval(toCalculate);
             }
-        } else if (operators.indexOf(buttonValue) > -1) {
-            var lastCharacter = inputValue[inputValue.length-1];
+        }
+        
+        else if (operators.indexOf(buttonValue) > -1) {
 
             if (inputValue !== "" && operators.indexOf(lastCharacter) === -1) {
                 input.innerHTML += buttonValue;
@@ -28,7 +37,9 @@ for (var i = 0; i < buttons.length; i++) {
             if (operators.indexOf(lastCharacter) > -1 && inputValue.length > 1) {
                 input.innerHTML = inputValue.replace(/.$/, buttonValue)
             }
-        } else {
+        }
+        
+        else {
             input.innerHTML += buttonValue;
         }
 
